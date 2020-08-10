50c0f7517952cb8bd1c8c21cdb4cf382117715cf
aleksandrgaranin <aleksandrgaranin89@gmail.com>
2020-08-10 12:55:54 -0700

    adjusted theme to blue and small design adjustments

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('its working')
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-	setTheme('light')
+	setTheme('blue')
 }else{
 	setTheme(theme)
 }
@@ -19,12 +19,12 @@ for (var i=0; themeDots.length > i; i++){
 }
 
 function setTheme(mode){
-    if(mode == 'light'){
+    if(mode == 'blue'){
         document.getElementById('theme-style').href = 'default.css'
     };
 
-    if(mode == 'blue'){
-        document.getElementById('theme-style').href = 'blue.css'
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'white.css'
     };
 
     if(mode == 'green'){
